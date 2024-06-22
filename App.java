7402a5242c0909e1fd40cda7dcce0832aa683393
Deepanjali Ishte <159618789+DeepanjaliIshte@users.noreply.github.com>
2024-06-22 16:50:16 +0530

    Update App.java

--- App.java
+++ App.java
@@ -4,20 +4,26 @@ public class App
 {
     public static void main(String[] args) throws Exception 
     {
+    	//Define Board Dimensions
+    			/**
+    			Sets the dimensions of the game board to 360 pixels wide and 640 pixels tall.
+    			**/
         int boardWidth = 360;
         int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
+     // Set JFrame Properties 
+        
         // frame.setVisible(true);
-		frame.setSize(boardWidth, boardHeight);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
+		frame.setSize(boardWidth, boardHeight);//Sets the size of the frame to match the game board dimensions.
+        frame.setLocationRelativeTo(null); // Centers the frame on the screen.
+        frame.setResizable(false); //// Prevents the user from resizing the frame.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        FlappyBird flappyBird = new FlappyBird();
-        frame.add(flappyBird);
-        frame.pack();
-        flappyBird.requestFocus();
-        frame.setVisible(true);
+        FlappyBird flappyBird = new FlappyBird();//Instantiates a FlappyBird object, which is the main game panel.
+        frame.add(flappyBird); 	//add game panel
+        frame.pack(); //Causes the frame to be resized to fit the preferred size and layouts of its subcomponents (in this case, the ChromeDinosaur panel).
+        flappyBird.requestFocus();   //Requests focus for the game panel to ensure it can capture keyboard input.
+        frame.setVisible(true); //Makes the frame visible on the screen.
     }
 }
